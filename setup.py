a074f3cbd430336b3f3c4e2d7fbd68fd1e3cc309
LB <tongjilibo@163.com>
2024-09-28 18:22:00 +0800

    v0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='bert4vector',
-    version='v0.0.4',
+    version='v0.0.5',
     description='an elegant bert4vector',
     long_description=long_description,
     long_description_content_type="text/markdown",
